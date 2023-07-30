2fe8a1ac5e39cfdd8f00f1ae20b7c23d988942a2
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-07-30 15:49:26 +0200

    EMBED tag implementation done

--- wp-content/themes/portfolio/home/project.php
+++ wp-content/themes/portfolio/home/project.php
@@ -6,7 +6,8 @@
             <img src="<?= get_field('project-img'); ?>" alt="exemple du site internet sur un Iphone">
             <div class="project__mockup__img">
                 <a href="https://clinicoeurs.renaud-vmb.com" title="" target="_blank">
-                    <img src="<?= get_field('project-img2'); ?>" alt="">
+                    <!--<img src="<?php /*= get_field('project-img2'); */?>" alt="représentation de la page d'accueil du site web">-->
+                    <embed type="text/html" src="http://clinicoeurs-asbl.test/fr/accueil/" width="115%" height="100%" style="transform: translate(-6.5%, 0);">
                 </a>
             </div>
         </div>
